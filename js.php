ac0bd61e13c9c50460d229858d968b39a9e3edf8
Yugesh <242237@slcs.edu.in>
2025-11-24 09:52:01 +0530

    hehe

--- js.php
+++ js.php
@@ -3,28 +3,46 @@ echo "
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-    <title>Document</title>
+    <title>JS Operator Exercises</title>
+    <style>
+        body { font-family: Arial; font-size: 18px; }
+        b { color: blue; }
+    </style>
 </head>
 <body>
-    <script>
-        let x = 10;
-        let y = 20;
-        let sum = x + y;
-        document.write('The sum of ' + x + ' and ' + y + ' is: ' + sum+'<br>');
-        // assignment operators
-        let a = 5;
-        a += 3; // equivalent to a = a + 3
-        document.write('<br>Value of a after += 3: ' + a);
-        // comparison operators
-        let p = 10;
-        let q = 20;
-        document.write('<br>Is p less than q? ' + (p < q));
-        // logical operators
-        let isAdult = true;
-        let hasID = false;
-        document.write('<br>Can enter club: ' + (isAdult && hasID));
-        
-        </script>
+
+<script>
+    // 1. Arithmetic Operators
+    let a = 20, b = 10;
+    document.write('<b>Arithmetic Operators</b><br>');
+    document.write('a + b = ' + (a + b) + '<br>');
+    document.write('a - b = ' + (a - b) + '<br>');
+    document.write('a * b = ' + (a * b) + '<br>');
+    document.write('a / b = ' + (a / b) + '<br>');
+    document.write('a % b = ' + (a % b) + '<br>');
+    document.write('a ** 2 = ' + (a ** 2) + '<br><br>');
+
+    // 2. Assignment Operators
+    let x = 10;
+    document.write('<b>Assignment Operators</b><br>');
+    x += 5; document.write('x += 5 => ' + x + '<br>');
+    x -= 2; document.write('x -= 2 => ' + x + '<br>');
+    x *= 3; document.write('x *= 3 => ' + x + '<br>');
+    x /= 2; document.write('x /= 2 => ' + x + '<br>');
+    x %= 4; document.write('x %= 4 => ' + x + '<br><br>');
+
+    // 3. Comparison Operators
+    document.write('<b>Comparison Operators</b><br>');
+    document.write('5 == \"5\" => ' + (5 == '5') + '<br>');
+    document.write('5 === \"5\" => ' + (5 === '5') + '<br>');
+    document.write('10 != 5 => ' + (10 != 5) + '<br>');
+    document.write('8 > 3 => ' + (8 > 3) + '<br>');
+    document.write('3 < 4 => ' + (3 < 4) + '<br>');
+    document.write('5 >= 5 => ' + (5 >= 5) + '<br>');
+    document.write('5 <= 6 => ' + (5 <= 6) + '<br><br>');
+
+</script>
+
 </body>
 </html>";
 ?>
